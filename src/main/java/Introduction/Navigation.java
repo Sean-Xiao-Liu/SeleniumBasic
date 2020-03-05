ad2ea4ba2f02f7beba078a186c4ecc9b3e8b5f44
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-03-05 11:58:24 -0500

    navigate methods, including to/back/forward/refresh

---
+++ src/main/java/Introduction/Navigation.java
@@ -0,0 +1,44 @@
+package Introduction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/** Navigation from one page to another page**/
+public class Navigation {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/seanliu/Documents/Web Driver/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        try{
+
+            // open the page of google
+            driver.get("https://www.google.com");
+            Thread.sleep(2000); // pause for 2 sec
+
+            // navigate to carfax
+            driver.navigate().to("https://www.carfax.com");
+            Thread.sleep(2000); // pause for 2 sec
+
+            // back to google
+            driver.navigate().back();
+            Thread.sleep(2000); // pause for 2 sec
+
+            // forward to carfax
+            driver.navigate().forward();
+            Thread.sleep(2000); // pause for 2 sec
+
+            // refresh the page
+            driver.navigate().refresh();
+            Thread.sleep(2000);
+
+
+        } catch (Exception e){
+            e.getMessage();
+        } finally {
+            driver.close();
+        }
+
+    }
+}
