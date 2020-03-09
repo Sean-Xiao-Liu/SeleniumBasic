b9e8097442369fa56ec71e3b6562f72bd53d4ec8
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-03-09 09:54:41 -0400

    drop down calendar, Select object

---
+++ src/main/java/Introduction/Calendar.java
@@ -0,0 +1,20 @@
+//package Introduction;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.Select;
+//
+//public class Calendar {
+//    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver","/Users/seanliu/Documents/Web Driver/chromedriver");
+//
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.get();
+//
+//        driver.findElement().click(); // find the drop down calendar and click the link
+//
+//        Select year = new Select(driver.findElement());
+//        year.selectByVisibleText("2020"); // select the value based on the shown drop list
+//    }
+//}
