286394305e689cadf015454902b050ea16ee2e11
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-03-11 12:54:47 -0400

    add switch cases

--- src/main/java/carfax/utilities/Driver.java
+++ src/main/java/carfax/utilities/Driver.java
@@ -1,4 +1,59 @@
 package carfax.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class Driver {
+
+    public Driver(){} // default constructor
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        if(driver == null){
+
+            // get browser which defined in configuration.properties
+            String browser=ConfigurationReader.get("browser");
+
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "chromeHeadless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "firefoxHeadless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    break;
+                case "safari":
+                    if (System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("You are using Windows as OS which doesn't support Safari");
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+            }
+        }
+
+        return driver;
+    }
+
+    public void closeDriver(){
+        if(driver != null){
+            driver.quit(); // close all tabs
+            driver = null;
+        }
+    }
 }
