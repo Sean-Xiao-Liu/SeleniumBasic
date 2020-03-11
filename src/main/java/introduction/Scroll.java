2f122d3bba4a7d662e13cd4c6b5dfd6f19bfe7f0
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-03-11 16:24:48 -0400

    update argument of scroll to element method

--- src/main/java/introduction/Scroll.java
+++ src/main/java/introduction/Scroll.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class Scroll{
-    public  void scrollToElement(WebElement element, WebDriver driver){
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+
+    WebDriver driver;
+
+    public  void scrollToElement(WebElement element){
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);");
     }
 
     public void scrollToBottom(WebDriver driver){
