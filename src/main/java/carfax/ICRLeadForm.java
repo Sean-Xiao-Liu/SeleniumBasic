ccaa54583d6c27f3b7ea4ca7537cd2916d0c9771
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-03-13 12:45:11 -0400

    all comments

--- src/main/java/carfax/ICRLeadForm.java
+++ src/main/java/carfax/ICRLeadForm.java
@@ -2,17 +2,20 @@ package carfax;
 
 import introduction.Scroll;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ICRLeadForm {
 
@@ -21,20 +24,43 @@ public class ICRLeadForm {
     public List<String> allTabs;
     public Scroll scroll = new Scroll();
     public LeadForm leadForm = new LeadForm();
+    public WebDriverWait wait;
 
+
+    public void driverSelection(String driverName){
+       switch (driverName){
+           case "chrome" :
+               WebDriverManager.chromedriver().setup();
+               driver=new ChromeDriver();
+               break;
+           case "chromeHeadless" :
+               WebDriverManager.chromedriver().setup();
+               driver=new ChromeDriver(new ChromeOptions().setHeadless(true));
+               break;
+           case "firefox":
+               WebDriverManager.firefoxdriver().setup();
+               driver=new FirefoxDriver();
+               break;
+           case "firefoxHeadless":
+               WebDriverManager.firefoxdriver().setup();
+               driver=new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+               break;
+           case "safari":
+               if(System.getProperty("os.name").toLowerCase().contains("windows"))
+                   throw  new WebDriverException("You are using Windows as OS which doesn't support Safari");
+               WebDriverManager.getInstance(SafariDriver.class).setup();
+               driver= new SafariDriver();
+               break;
+       }
+    }
 
     public void stepOnePage(){
-
-
-        WebDriverManager.chromedriver().setup();
-
-        driver = new ChromeDriver();
 
         /** setting window size and wait time**/
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();// maximize the window when open a new one
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // wait 10 sec before throwing "No Such Element Exception"
+//        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 
 
         driver.get("https://carfax.com/cars-for-sale");
@@ -64,41 +90,34 @@ public class ICRLeadForm {
         WebElement zip = driver.findElement(By.xpath("//input[@name='zip']"));
         WebElement submit = driver.findElement(By.xpath("//button[@type='submit']"));
 
-        try{
-            // input invalid zip code and check the pop up
-            zip.sendKeys("22213");
-            submit.click();
-        } catch (Exception e){
-            e.getMessage();
-        }
+        zip.sendKeys("22213");
+        submit.click();
     }
 
-    public void stepTwoPage(){
+    public void stepTwoPage() throws InterruptedException {
         // on step 2 page, then click the show me result button
-        WebElement showResults = driver.findElement(By.xpath("//button[@class='button large primary-green']"));
-        if(showResults.isDisplayed() && showResults.isEnabled()){
+        WebElement showResults = driver.findElement(By.cssSelector("button[class='button large primary-green']"));
+//        WebElement showResults = driver.findElement(By.cssSelector("div.main-wrapper div.spa-container:nth-child(1) div.div-ui-view:nth-child(2) div.home:nth-child(1) div.top-section div.row.align-center.align-middle.home-search-row:nth-child(3) div.column:nth-child(1) div.searchForm-wrapper.homepage div.four-pillar-form.form-padding footer.row.align-middle div.small-12.medium-8.medium-order-2.align-middle.float-right.columns > button.button.large.primary-green"));
+
+        wait = new WebDriverWait(driver, 2); // Explicit wait, wait for Expected Condition
+      //  if(showResults.isDisplayed() && showResults.isEnabled()){
+// wait.until(ExpectedConditions.elementToBeClickable(showResults));
+        Thread.sleep(2000);
             showResults.click();
             System.out.println("show me results button clicked");
-        } else {
-            System.out.println("sth wrong with the show me results button");
-        }
-
+      //  } else {
+           // System.out.println("sth wrong with the show me results button");
+     //   }
     }
 
 
     public void switchTab(){
-//        String oldTab = driver.getWindowHandle(); // get current window handle
+        // get a list of all current handles available
         allTabs = new LinkedList<>(driver.getWindowHandles());
-
-//        System.out.println(oldTab);
-//        for(int i = 0 ; i < allTabs.size() ; i++){
-//            System.out.println(allTabs.get(i));
-//        }
     }
 
-    public void sendMessageToDealer(){
+    public void sendMessageToDealer() throws ElementNotVisibleException,ElementNotSelectableException{
 
-        try {
             //input first name
             WebElement firstName = driver.findElement(By.name("firstName"));
             firstName.sendKeys("FirstName");
@@ -111,20 +130,13 @@ public class ICRLeadForm {
 
             //send message
             driver.findElement(By.xpath("//button[@class='search-submit button large expanded primary-green']")).click();
-
-            Thread.sleep(3000);
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
     }
 
 
 
-    public void findICR(){
+    public void findICR() throws ElementNotVisibleException, ElementNotSelectableException, InterruptedException {
 
-        try {
             stepOnePage();
-            Thread.sleep(3000);
             stepTwoPage();
             driver.findElement(By.xpath("//span[contains(text(),'2019 Audi A5 Premium Plus')]")).click();
 
@@ -132,25 +144,19 @@ public class ICRLeadForm {
             driver.switchTo().window(allTabs.get(allTabs.size()-1));
 
             scroll.scrollToBottom(driver);
+            scroll.scrollToElement(driver.findElement(By.xpath("//h4[@class='sellerDescription-title false false']")),driver);
 
             scroll.scrollToElement(driver.findElement(By.xpath("//div[@class='vdp-container-body']//a[@class='button large'][contains(text(),'View FREE CARFAX Report')]")),driver); // have to switch tab before find element
-            Thread.sleep(2000);
             driver.findElement(By.xpath("//div[@class='vdp-container-body']//a[@class='button large'][contains(text(),'View FREE CARFAX Report')]")).click();
 
-//            allTabs = new LinkedList<>(driver.getWindowHandles());
             switchTab();
 
             driver.switchTo().window(allTabs.get(allTabs.size()-1)); // switch to the new tab
-//            if(driver.getWindowHandle().equals(allTabs.get(2))){
-//                System.out.println("tab switch successful");
-//            }
-
-
-//            scroll.scrollToBottom(driver);
 
 
             // wait for iframe
-            WebDriverWait wait = new WebDriverWait(driver, 10);
+
+//            WebDriverWait wait = new WebDriverWait(driver, 10);
 
             //Find frame or iframe and switch
             wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt("uclLeadFrame"));
@@ -166,23 +172,30 @@ public class ICRLeadForm {
             } else {
                 System.out.println("Lead form is not available/displayed");
             }
-
-//            leadForm.isLeadFormEnabled();
-
-            Thread.sleep(3000);
-
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-
-        } finally {
+Thread.sleep(10000);
             System.out.println("The test is finished!");
             driver.quit();
-        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<String> driverNamesList = new ArrayList<>();
+        driverNamesList.add("chrome");
+//        driverNamesList.add("firefox");
+//        driverNamesList.add("safari");
+//        driverNamesList.add("chromeHeadless");
+//        driverNamesList.add("firefoxHeadless");
+
         ICRLeadForm test = new ICRLeadForm();
-        test.findICR();
+
+
+        // ues for loop to
+        for(int i = 0; i < driverNamesList.size() ; i++){
+            test.driverSelection(driverNamesList.get(i));
+            test.findICR();
+        }
+
+
 
     }
 }
