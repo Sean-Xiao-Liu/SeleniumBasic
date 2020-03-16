e6819e7b4bc583626c809f6ddeeee64bbdbcb9dd
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-03-16 09:41:26 -0400

    remove Web driver wait element

--- src/main/java/carfax/pages/StepTwoPage.java
+++ src/main/java/carfax/pages/StepTwoPage.java
@@ -1,4 +1,20 @@
 package carfax.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class StepTwoPage {
+
+    private static WebDriver driver;
+    public WebElement showResults;
+
+    public StepTwoPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void clickShowMeResults(){
+        showResults = driver.findElement(By.cssSelector("button[class='button large primary-green']"));
+        showResults.click();
+    }
 }
