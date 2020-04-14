4e11a5437f460434045d3d567155e0c2ccc22632
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-04-14 15:18:44 -0400

    definition of test procedure

--- src/test/java/stepDefinitions/FacebookLoginStepDef.java
+++ src/test/java/stepDefinitions/FacebookLoginStepDef.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.junit.Cucumber;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,7 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-@RunWith(Cucumber.class)
+
 public class FacebookLoginStepDef {
 
     public WebDriver driver = DriverConfiguration.chooseDriver("chrome");
@@ -22,21 +23,28 @@ public class FacebookLoginStepDef {
 
     @Given("User is on facebook home page")
     public void userIsOnFacebookHomePage() {
+        driver.get("https://www.google.com");
     }
 
     @And("User input username and password")
     public void userInputUsernameAndPassword() {
-
+        driver.get("https://www.google.com");
     }
 
     @When("User click the login button")
-    public void userClickTheLoginButton(){
-        driver.get("https://www.facebook.com/");
-        WebElement loginButton = driver.findElement(By.id("loginbutton"));
-        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+    public void userClickTheLoginButton() throws InterruptedException {
+        Thread.sleep(3000);
+        WebElement loginButton = driver.findElement(By.xpath("//input[@name='q']"));
+//        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+        loginButton.sendKeys("try search this");
     }
 
     @Then("User should be logged in")
     public void userShouldBeLoggedIn() {
+        WebElement googleSearchButton = driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']"));
+        googleSearchButton.click();
+        // assert the google logo is displayed
+        Assert.assertTrue(1 == 1);
+        DriverConfiguration.closeDriver();
     }
 }
