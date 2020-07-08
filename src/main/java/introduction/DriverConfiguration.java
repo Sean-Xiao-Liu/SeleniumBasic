99f96040082016cc4a3de880072b1e56be11088b
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-07-08 13:20:57 -0400

    add get driver method without no argument

--- src/main/java/introduction/DriverConfiguration.java
+++ src/main/java/introduction/DriverConfiguration.java
@@ -1,5 +1,6 @@
 package introduction;
 
+import carfax.utilities.ConfigurationReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
@@ -17,6 +18,38 @@ public class DriverConfiguration {
 
     public static WebDriver chooseDriver(String name){
 
+        switch (name){
+            case "chrome" :
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+                break;
+            case "chromeHeadless" :
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver(new ChromeOptions().setHeadless(true));
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+                break;
+            case "firefoxHeadless":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                break;
+            case "safari":
+                if(System.getProperty("os.name").toLowerCase().contains("windows"))
+                    throw  new WebDriverException("You are using Windows as OS which doesn't support Safari");
+                WebDriverManager.getInstance(SafariDriver.class).setup();
+                driver= new SafariDriver();
+                break;
+        }
+
+        return driver;
+    }
+
+
+    public static WebDriver getDriver(){
+        String name = ConfigurationReader.get("browser");
+        System.out.println("The chosen browser is : " + name);
         switch (name){
             case "chrome" :
                 WebDriverManager.chromedriver().setup();
